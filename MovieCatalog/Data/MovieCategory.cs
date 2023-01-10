56352fdd01b7fc886c98bdbfd512e0af98bf40c0
alexander-slavchev <a.slavchev19.48@gmail.com>
2023-01-10 22:30:16 +0200

    Models added

---
+++ MovieCatalog/Data/MovieCategory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieCatalog.Data
+{
+    public enum MovieCategory
+    {
+        Comedy = 1,
+        Drama,
+        Action,
+        Documentary,
+        Historical,
+        Horror
+
+
+    }
+}
---
+++ MovieCatalog/Models/Actor.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieCatalog.Models
+{
+    public class Actor
+    {
+        [Key]
+        public int ID { get; set; }
+        public string ProfilePicURL { get; set; }
+        public string FullName { get; set; }
+        public string Bio { get; set; }
+        public string MoviesPlayedIn { get; set; }
+    }
+}
